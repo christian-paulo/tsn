4551dbbe7503e48db3ec5289496c332168aef11e
ChristianPaulo <christianpaulodev@gmail.com>
2022-08-09 10:27:05 -0300

    First Commit

---
+++ tsn/Api.swift
@@ -0,0 +1,8 @@
+//
+//  Api.swift
+//  tsn
+//
+//  Created by Christian Paulo on 09/08/22.
+//
+
+import Foundation
---
+++ tsn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tsn
+//
+//  Created by Christian Paulo on 09/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var Title: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
