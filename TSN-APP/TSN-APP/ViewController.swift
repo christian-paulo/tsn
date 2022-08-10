89c727ec0950e4ca9a7f1d5223cd32fc83d95479
Tales Valente <talesvalente@gmail.com>
2022-08-10 10:46:09 -0300

    Content Update
    Teste

---
+++ TSN-APP/TSN-APP/ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  TSN-APP
+//
+//  Created by Tales Valente on 09/08/22.
+//
+// Do any additional setup after loading the view.
+
+/*
+Pair Programming ou Dojo
+Implementar Cadastro de Usuário
+Implementar Login & Logout
+Implementar Remoção de Usuário
+Implementar Criação de Post com texto simples
+Armazenar Token de autenticação de maneira segura
+Consumir pelo menos uma rota usando CompletionHandler
+Consumir pelo menos uma rota usando Async/Await
+Persistir dados com UserDefaults
+Fazer testes unitários da camada de redes
+*/
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let url = "http://api.sunrise-sunset.org/json?date=2020-01-01&lat=-74.0060&lng=40.7128&formatted=0"
+        getData(from: url)
+    }
+    private func getData(from url: String) {
+       let task = URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: { data, response, error in
+            
+            guard let data = data, error == nil else {
+                print ("Something went wrong")
+                return
+            }
+            //have data
+            var result: Response?
+            do {
+                result = try JSONDecoder().decode(Response.self, from: data)
+            }
+            catch {
+                print ("Failed to convert \(error.localizedDescription)")
+                
+            }
+            
+            guard let json = result else {
+                return
+            }
+            
+            print(json.status)
+            print(json.results.sunrise)
+            print(json.results.sunset)
+            print(json.results.solar_noon)
+            
+        })
+            task.resume()
+        
+    }
+}
+
+
+struct Response: Codable {
+    let results: MyResult
+    let status: String
+}
+
+struct MyResult: Codable {
+    let sunrise: String
+    let sunset: String
+    let solar_noon: String
+    let day_length: Int
+    let civil_twilight_begin: String
+    let civil_twilight_end: String
+    let nautical_twilight_begin: String
+    let nautical_twilight_end: String
+    let astronomical_twilight_begin: String
+    let astronomical_twilight_end: String
+    
+}
